ffefb05865fd477239947d2c8a7721eb19d60251
YOUSIF HUSSEIN JABBAR AL-GBURI <yuosefh2005@gmail.com>
2025-12-04 00:43:14 +0300

    implementation of Customer header v1.1

---
+++ src/payment_methods/Payment.cpp
@@ -0,0 +1,20 @@
+#include "Payment.h"
+
+using namespace std;
+
+Payment::Payment(double newamount) {
+	if (newamount < 0) { this->amount = 0; }
+	else{ this->amount = newamount; }
+}
+
+double Payment::getAmount() {
+	return this->amount;
+}
+
+void Payment::setAmount(double newamount) {
+	if (newamount < 0) { this->amount = 0; }
+	else {
+		this->amount = newamount;
+	}
+}
+
