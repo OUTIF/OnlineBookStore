b65212a1bd728edfb23eee3ad490019afa5d6687
YOUSIF HUSSEIN JABBAR AL-GBURI <yuosefh2005@gmail.com>
2025-12-06 18:45:21 +0300

    version 1.2 of the Product header

--- src/core/Product.h
+++ src/core/Product.h
@@ -1,12 +1,4 @@
-/**
-* İsim: İbrahim Kütük
-* Tarih: 5/12/2025
-* Dosya: Product.h
-* Açıklama: Tüm ürünlerin türetileceği soyut temel sınıf (Abstract Base Class).
-*/
-
-#ifndef PRODUCT_H
-#define PRODUCT_H
+#pragma once
 
 #include <string>
 #include <iostream>
@@ -15,19 +7,19 @@ using namespace std;
 
 class Product {
 private:
-    // UML diyagramındaki temel özellikler
+    
     int id;
     string name;
     double price;
 
 public:
-    // Yapıcı (Constructor)
-    Product(int id, string name, double price);
     
-    // Sanal Yıkıcı (Virtual Destructor) - Kalıtım için gereklidir
+    Product(int id, string name, double price);
+
+
     virtual ~Product();
 
-    // Getter ve Setter Metodları
+ 
     int getID() const;
     void setID(int id);
 
@@ -37,9 +29,5 @@ public:
     double getPrice() const;
     void setPrice(double price);
 
-    // Saf Sanal Fonksiyon (Pure Virtual Function)
-    // Bu sınıfı soyut (abstract) yapar. Alt sınıflar bunu ezmek (override) zorundadır.
-    virtual void printProperties() const = 0;
+    virtual void printProperties();
 };
-
-#endif
